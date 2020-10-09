b4dd3eb134ea3749d6b8f7ef75b606b3702d1d91
Iriska-A <72428513+Iriska-A@users.noreply.github.com>
2020-10-09 19:49:33 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace magazin
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Mag magg = new Mag("Введите имя", "Введите фамилию", "Введите Email", "Придумайте пароль", "Зарегистрироваться");
+
+            Console.WriteLine(magg.ReturnSurnameMag());
+
+
+            Console.WriteLine(magg.ReturnNameMag());
+            Console.WriteLine(magg.ReturnPasswordMag());
+            Console.WriteLine(magg.ReturnEmailMag());
+            Console.WriteLine(magg.ReturnInputMag());
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
